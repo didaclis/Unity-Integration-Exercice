b749ecaa743a2a3337e22e920b110c934c4c8f60
Enric <36191828+PerezEnric@users.noreply.github.com>
2020-05-11 19:46:07 +0200

    Audio Mixer

---
+++ WAG_No_Sound/Assets/SetVolume.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer audio_mixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMasterVolume(float slider_value)
+    {
+        audio_mixer.SetFloat("Master_Volume", Mathf.Log10(slider_value)*20);
+    }
+
+    public void SetMusicVolume(float slider_value)
+    {
+        audio_mixer.SetFloat("Music_Volume", Mathf.Log10(slider_value) * 20);
+    }
+}
