09449704beda79a2e72a66b4bb20c6d482bd5eb4
RoperoIvan <ivan_ropero98@hotmail.com>
2020-05-14 12:54:41 +0200

    ambientsheitfixed

---
+++ WAG_No_Sound/Assets/ManageEnv.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageEnv : MonoBehaviour
+{
+    public List<GameObject> Envis;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void DisableAllExcept(GameObject chosen_one)
+    {
+        foreach (GameObject g_o in Envis)
+        {
+            if(g_o != chosen_one)
+            {
+                g_o.GetComponent<ChoseAmbient>().DisableChilds();
+            }
+            else
+            {
+                g_o.GetComponent<ChoseAmbient>().EnableChilds();
+            }
+        }
+    }
+}
--- WAG_No_Sound/Assets/Scripts/World/ChoseAmbient.cs
+++ WAG_No_Sound/Assets/Scripts/World/ChoseAmbient.cs
@@ -6,23 +6,73 @@ public class ChoseAmbient : MonoBehaviour
 {
     public GameObject day_music;
     public GameObject night_music;
-
+    public GameObject day_music_real;
+    public GameObject night_music_real;
     public GameObject sun;
-
     // Update is called once per frame
-    void Update()
+    void ManageAudio()
     {
-        if(sun.GetComponent<DayNightCycle>().timeOfDay >= 21.1f && sun.GetComponent<DayNightCycle>().timeOfDay <= 21.2f)
+        if (day_music == day_music_real)
+            return;
+        if (sun.GetComponent<DayNightCycle>().timeOfDay >= 21.1f && sun.GetComponent<DayNightCycle>().timeOfDay <= 21.2f)
         {
             day_music.SetActive(false);
             night_music.SetActive(true);
             night_music.GetComponent<AudioSource>().Play();
+            day_music_real.SetActive(false);
+            night_music_real.SetActive(true);
+            night_music_real.GetComponent<AudioSource>().Play();
         }
         else if (sun.GetComponent<DayNightCycle>().timeOfDay >= 6.1f && sun.GetComponent<DayNightCycle>().timeOfDay <= 6.2f)
         {
             night_music.SetActive(false);
             day_music.SetActive(true);
             day_music.GetComponent<AudioSource>().Play();
+            night_music_real.SetActive(false);
+            day_music_real.SetActive(true);
+            day_music_real.GetComponent<AudioSource>().Play();
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.parent.GetComponent<ManageEnv>().DisableAllExcept(gameObject);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ManageAudio();
+        }
+    }
+    public void EnableChilds()
+    {
+        if (sun.GetComponent<DayNightCycle>().timeOfDay >= 6.1f && sun.GetComponent<DayNightCycle>().timeOfDay <= 21.2f)
+        {
+            night_music.SetActive(false);
+            day_music.SetActive(true);
+            day_music.GetComponent<AudioSource>().Play();
+            night_music_real.SetActive(false);
+            day_music_real.SetActive(true);
+            day_music_real.GetComponent<AudioSource>().Play();
+        }      
+        else
+        {
+            day_music.SetActive(false);
+            night_music.SetActive(true);
+            night_music.GetComponent<AudioSource>().Play();
+            day_music_real.SetActive(false);
+            night_music_real.SetActive(true);
+            night_music_real.GetComponent<AudioSource>().Play();
+        }
+    }
+    public void DisableChilds()
+    {
+        day_music.SetActive(false);
+        night_music.SetActive(false);
+        day_music_real.SetActive(false);
+        night_music_real.SetActive(false);
     }
 }
